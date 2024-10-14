f4ee322a78bc82bad969883c0965e7f3aed6f644
gentootoaster <lobachev.arseniy@gmail.com>
2024-10-14 14:29:13 +0500

    Add files via upload

---
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,28 @@
+-- Rose pine theme
+
+--return {
+-- -- add rose-pine
+--  { "rose-pine/neovim" },
+--
+--  -- Configure LazyVim to load rose-pine
+--  {
+--    "LazyVim/LazyVim",
+--    opts = {
+--      colorscheme = "rose-pine",
+--    },
+--  }
+--}
+
+return {
+    -- add molokai
+     {"UtkarshVerma/molokai.nvim"},
+
+     -- Configurate LazyVim to load molokai
+     {
+       "LazyVim/LazyVim",
+       opts = {
+         colorscheme = "molokai",
+       },
+    }
+ }
+
